aa03d01a58a31577c5d19db90c3b57c119b4228e
Greg <gergodemeny@gmail.com>
2021-01-29 23:22:37 +0200

    Simplify db.

--- db/db.js
+++ db/db.js
@@ -1,26 +1,11 @@
-const low = require('lowdb'),
-  FileSync = require('lowdb/adapters/FileSync'),
-  fs = require('fs'),
-  data = require('../data/db.json');
+const data = require('../data/db.json');
 
-const defaultDb = data;
-
-if (!fs.existsSync('../data')) {
-  fs.mkdirSync('../data');
-}
-
-const db = low(new FileSync('data/db.json'), {
-  serialize: (obj) => JSON.stringify(obj),
-  deserialize: (obj) => JSON.parse(obj),
-});
-
-db.defaults(defaultDb).write();
-
-exports.findAllEditions = () => db.get('editions').value();
-
+exports.findAllEditions = () => data.editions;
 exports.findCardsByEdition = (editions = []) => {
   if (Array.isArray(editions)) {
-    return db.get('cards').value().filter((card) => editions.includes(card.editionId)).map((card) => ({ id: card.id, fileName: card.fileName }));
+    return data.cards
+      .filter((card) => editions.includes(card.editionId))
+      .map((card) => ({ id: card.id, fileName: card.fileName }));
   }
   throw Error('Invalid editions.');
 };
